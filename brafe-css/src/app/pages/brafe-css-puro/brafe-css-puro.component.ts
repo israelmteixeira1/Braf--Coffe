4ab8b112a150cf22cbca6e15fab20c4072732478
Israel M Teixeira <israelmteixeira1@gmail.com>
2022-09-19 12:16:14 -0300

    feat: Changed Brafe project to a angular project

---
+++ brafe-css/src/app/pages/brafe-css-puro/brafe-css-puro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-brafe-css-puro',
+  templateUrl: './brafe-css-puro.component.html',
+  styleUrls: ['./brafe-css-puro.component.css']
+})
+export class BrafeCssPuroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
